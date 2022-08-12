5651e61df422a6f5108a04622c1eb1c3d00cf52c
luizcarrijo <88462816+luizcarrijo@users.noreply.github.com>
2022-08-11 22:06:40 -0300

    Add files via upload

---
+++ ProjetoPW/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Projeto PHP</title>
+    </head>
+    <body>
+        <table>
+                <tr>
+                    <td>
+                        <h3><img src="\ProjetoPW\botoes\imgEmpresa.jpg" title="Empresa Softron"></h3>
+                    </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#047AC4"></td>
+                </tr>
+        </table>
+    </body>
+</html>
